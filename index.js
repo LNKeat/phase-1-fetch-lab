32946db592eb3565332f2dfb599bb1c9ff59bb33
Laura <lnkeat@gmail.com>
2021-07-21 13:45:37 -0600

    commit tree 5d004113fd306a425e8474084378656ec24aca56

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+  return fetch('https://anapioficeandfire.com/api/books')
+  .then(resp => resp.json())
+  .then(json => renderBooks(json)); 
 }
 
 function renderBooks(books) {
@@ -8,6 +10,7 @@ function renderBooks(books) {
   books.forEach(book => {
     const h2 = document.createElement('h2');
     h2.innerHTML = book.name;
+    h2.id = book.isbn
     main.appendChild(h2);
   });
 }
